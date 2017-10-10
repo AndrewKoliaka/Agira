6939bc474fe02351d0883157cfd3ef1095bbb32d
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-10-10 16:57:54 +0300

    adaptive added

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,34 @@ $(document)
         $('.slider').slick();
 
 
-        $('.control--left').click(function(event){
+        $('.control--left').click(function (event) {
             event.preventDefault();
             $('.slider').slick('slickPrev');
         });
 
-        $('.control--right').click(function(event){
+        $('.control--right').click(function (event) {
             event.preventDefault();
             $('.slider').slick('slickNext');
         });
+
+        var toggleMenu = $('.header-menu');
+
+        $('.site-nav__menu-trigger').click(function () {
+            toggleMenu.slideToggle(200);
+        });
+
+        function adaptiveToScreenWidth() {
+            var screenWidth = $(window).width();
+
+            if (screenWidth < 768) {
+                toggleMenu.removeAttr('style');
+            }
+        }
+
+        $(window)
+            .resize(function () {
+                adaptiveToScreenWidth();
+            });
+
+        adaptiveToScreenWidth();
     });
