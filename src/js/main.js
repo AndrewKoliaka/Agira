d95caffb1ba195bae875d6f691e3ac212f607f9d
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-10-10 14:53:34 +0300

    push

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 $(document)
     .ready(function () {
         $('.slider').slick();
+
+
+        $('.control--left').click(function(event){
+            event.preventDefault();
+            $('.slider').slick('slickPrev');
+        });
+
+        $('.control--right').click(function(event){
+            event.preventDefault();
+            $('.slider').slick('slickNext');
+        });
     });
